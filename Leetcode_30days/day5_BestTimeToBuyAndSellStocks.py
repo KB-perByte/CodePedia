47fc6989ec851e6cafaadd0c2eb80abc7724781c
Sagar Paul <paul.sagar@yahoo.com>
2020-04-05 15:53:58 +0530

    Day5 leet

---
+++ Leetcode_30days/day5_BestTimeToBuyAndSellStocks.py
@@ -0,0 +1,20 @@
+'''
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+'''
+
+
+class Solution(object):
+    def maxProfit(self, A):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        totProf = 0
+        for i in range(1, len(A)):
+            totProf += max(0, A[i] - A[i - 1])
+        return totProf
+        
