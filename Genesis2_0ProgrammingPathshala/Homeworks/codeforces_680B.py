e18dd352835d1e07fa1495c24f32c6888638d291
Sagar Paul <paul.sagar@yahoo.com>
2020-04-20 12:48:19 +0530

    renamed

---
+++ Genesis2_0ProgrammingPathshala/Homeworks/codeforces_680B.py
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int nax = 1005;
+int t[nax];
+int main() {
+	int n, a;
+	scanf("%d%d", &n, &a);
+	for(int i = 1; i <= n; ++i)
+		scanf("%d", &t[i]);
+	int answer = 0;
+	for(int i = 1; i <= n; ++i) if(t[i]) {
+		// can we catch criminal in city i?
+		int distance = i - a; // distance from a
+		int j = a - distance; // the other city at the same distance
+		if(j < 1 || j > n || t[i] == t[j])
+			++answer;
+	}
+	printf("%dn", answer);
+	return 0;
+}
