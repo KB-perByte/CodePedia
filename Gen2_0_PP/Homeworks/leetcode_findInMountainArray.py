e4314988ac4b7c4171f695cec4c8d5bf1e40c0a4
KB-perByte <paul.sagar@yahoo.com>
2020-06-21 23:25:06 +0530

    dat22

--- Gen2_0_PP/Homeworks/leetcode_findInMountainArray.py
+++ Gen2_0_PP/Homeworks/leetcode_findInMountainArray.py
@@ -18,22 +18,20 @@ class Solution(object):
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         leng = mountain_arr.length()
         left, right = 0, leng-1
-        # find peak
-        while left < right: # there is definitely a peak, so we use '<' instead of '<='
+        
+        while left < right:
             mid = (left+right) // 2
             if mountain_arr.get(mid) >= mountain_arr.get(mid+1):
                 right = mid
             else:
-                # 'left' and 'right' could be consecutive
-                # 'mid' intends to be 'left', so we need 'left' to plus one to avoid endless loop
                 left = mid + 1 
         peak = left
         if target == mountain_arr.get(peak):
             return peak
 
-        # search behind the peak
+       
         l, r = 0, peak-1
-        while l <= r: # there may not be a target behind the peak, so using '<='
+        while l <= r:
             print(l, r, end =' ')
             mid = (l + r) // 2
             print('1:', mid)
@@ -45,9 +43,9 @@ class Solution(object):
             else:
                 l = mid + 1
                 
-        # search after the peak
+        
         l, r = peak+1, leng-1
-        while l <= r: # there may not be a target after the peak, so using '<='
+        while l <= r:
             print('2:', mid)
             mid = (l + r) // 2
             cur = mountain_arr.get(mid)
